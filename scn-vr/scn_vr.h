f410baa25d9e820e1d8912f12780ea2146a0da82
Michael Fuller <mmgate@hotmail.com>
2014-12-22 23:28:27 -0600

    Initial Commit

---
+++ scn-vr/scn_vr.h
@@ -0,0 +1,13 @@
+//
+//  scn_vr.h
+//  scn-vr
+//
+//  Created by Michael Fuller on 12/22/14.
+//  Copyright (c) 2014 M-Gate Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface scn_vr : NSObject
+
+@end
---
+++ scn-vr/scn_vr.m
@@ -0,0 +1,13 @@
+//
+//  scn_vr.m
+//  scn-vr
+//
+//  Created by Michael Fuller on 12/22/14.
+//  Copyright (c) 2014 M-Gate Labs. All rights reserved.
+//
+
+#import "scn_vr.h"
+
+@implementation scn_vr
+
+@end
